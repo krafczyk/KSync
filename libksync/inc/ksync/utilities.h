adcf6dbe7c3cd38b6fd7b98528da24e8d43224ce
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2017-05-25 15:02:32 -0500

    Add trim utility functions

--- libksync/inc/ksync/utilities.h
+++ libksync/inc/ksync/utilities.h
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define KSYNC_UTILITIES_HDR
 
 #include <string>
+#include <algorithm>
 #include "ArgParse/ArgParse.h"
 
 namespace KSync {
@@ -34,6 +35,19 @@ namespace KSync {
 		typedef unsigned long client_id_t;
 		client_id_t GenerateNewClientId();
 		int get_client_socket_url(std::string& dir, const client_id_t client_id);
+		static inline std::string &ltrim(std::string &s) {
+			s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+				std::not1(std::ptr_fun<int, int>(std::isspace))));
+			return s;
+		}
+		static inline std::string &rtrim(std::string &s) {
+			s.erase(std::find_if(s.rbegin(), s.rend(),
+				std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+			return s;
+		}
+		static inline std::string &trim(std::string &s) {
+			return ltrim(rtrim(s));
+		}
 	}
 }
 
