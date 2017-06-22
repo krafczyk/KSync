7f920bd12dbdfad9103d3efc372f3c199f66fb94
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2017-06-21 19:56:04 -0500

    Partial work on lock free queue

--- core/inc/ksync/thread_utilities.h
+++ core/inc/ksync/thread_utilities.h
@@ -181,6 +181,78 @@ namespace KSync {
 					}
 				}
 		};
+
+		//Thread-safe queue with a busy-wait queue (not truely lock-free)
+		template<typename T>
+		class threadsafe_lock_free_queue {
+			private:
+				struct node {
+					std::shared_ptr<T> data;
+					std::shared_ptr<node> next;
+					node(const T& data_):
+						data(std::make_shared<T>(data_)) {
+					}
+				};
+
+				std::shared_ptr<node> head;
+				std::shared_ptr<node> tail;
+
+				/*std::shared_ptr<node> pop_head() {
+					const std::shared_ptr<node> old_head = std::atomic_load(head);
+					if(old_head == std::atomic_load(tail)) {
+						return std::shared_ptr<node>();
+					}
+					std::atomic_store(head, old_head->next);
+					return old_head;
+				}*/
+			public:
+				threadsafe_lock_free_queue() : head(std::move(std::make_shared(new node))) , tail(std::atomic_load(head)) {
+				}
+				threadsafe_lock_free_queue(const threadsafe_lock_free_queue& rhs) = delete;
+				threadsafe_lock_free_queue& operator=(const threadsafe_lock_free_queue& rhs) = delete;
+				~threadsafe_lock_free_queue() {
+					while(const std::shared_ptr<node> old_head = std::atomic_load(head)) {
+						std::atomic_store(head, old_head->next);
+						//std::atomic_store(old_head, nullptr);
+					}
+				}
+
+				std::shared_ptr<T> pop() {
+					//Same solution as in lock-free stack for pop.
+					std::shared_ptr<node> old_head = std::atomic_load(head);
+					while(old_head && !std::atomic_compare_exchange_weak(head,
+						old_head, old_head->next));
+					return old_head ? old_head->data : std::shared_ptr<T>();
+				}
+
+				void push(std::shared_ptr<T>& new_value) {
+					//Create new data
+					std::shared_ptr<T> new_data(std::move(new_value));
+					//Create new empty node for end of queue.
+					std::shared_ptr<node> p(new node);
+					while (true) {
+						//Create emtpy data pointer
+						std::shared_ptr<T> expected_old_data;
+						//Get current tail.
+						const std::shared_ptr<node> old_tail = std::atomic_load(tail);
+						//Compare that empty data pointer to that stored in tail.
+						//It should be empty since tail should point to an empty node.
+						//Exchange it's content for the new data we created.
+						//We do this first so that node's data is prepared.
+						if(std::atomic_compare_exchange_strong(old_tail->data, expected_old_data, new_data)) {
+							std::shared_ptr<node> null_next;
+							if(std::atomic_compare_exchange_strong(old_tail->next, null_next, p
+							//Set tail's next pointer to new empty node.
+							old_tail->next = p;
+							//atomically store empty node as new tail..
+							std::atomic_store(tail, p);
+							//old version:
+							//old_tail = std::atomic_exchange(tail, p);
+							break;
+						}
+					}
+				}
+		};
 	}
 }
 
